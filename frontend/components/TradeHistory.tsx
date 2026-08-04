def872ad8e5ac4f38c353536d22619669a1e63da
JiHo Park <devcrupy@gmail.com>
2026-08-05 01:16:53 +0900

    Create TradeHistory.tsx

---
+++ frontend/components/TradeHistory.tsx
@@ -0,0 +1,47 @@
+export default function TradeHistory(){
+
+return(
+
+<div className="card">
+
+<h2>최근 거래</h2>
+
+<br/>
+
+<table>
+
+<tbody>
+
+<tr>
+
+<td>SOXL</td>
+
+<td className="green">+5.42%</td>
+
+</tr>
+
+<tr>
+
+<td>SOXS</td>
+
+<td className="red">-2.14%</td>
+
+</tr>
+
+<tr>
+
+<td>TQQQ</td>
+
+<td className="green">+3.11%</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+</div>
+
+)
+
+}
