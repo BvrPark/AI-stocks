914d26124b8ed6912725e7a850fc20df61091c77
JiHo Park <devcrupy@gmail.com>
2026-08-05 01:12:06 +0900

    Create Header.tsx

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,29 @@
+export default function Header() {
+
+    return(
+
+        <header className="header">
+
+            <div>
+
+                <h1>Dashboard</h1>
+
+                <p>Investment Operating System</p>
+
+            </div>
+
+            <div>
+
+                <button>
+
+                    로그인
+
+                </button>
+
+            </div>
+
+        </header>
+
+    )
+
+}
